9a84e86a82fce34db50bb6d5a6643c44449d3973
B00131319 Maksymilian Krawczyk <B00131319@mytudublin.ie>
2024-01-31 12:37:03 +0000

    all 3 parts

---
+++ PartA.py
@@ -0,0 +1,9 @@
+#Max
+#30/01/2024
+#A Program to Print Steps 1 to 9 to the Screen
+
+def printSteps(a, b):
+    for i in range(a, b+1):
+        print(f"Steps {i}")
+
+printSteps(1,9)
---
+++ PartC.py
@@ -0,0 +1,16 @@
+#Max
+#30/01/2024
+#A Program to Print Steps 1 to 9 to the Screen
+
+def printSteps():
+        print("Steps 4")
+        print("Steps 5")
+        print("Steps 6")
+
+print("Step 1")
+print("Step 2")
+print("Step 3")
+printSteps()
+print("Step 7")
+print("Step 8")
+print("Step 9")
